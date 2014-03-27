4a7334e10aae25ad9b8329f7572c121a6196c843
Gunnar Aastrand Grimnes <gunnar@bakkenbaeck.no>
2014-03-27 21:45:48 +0100

    check that _session exists

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='BBTornado',
-    version='0.0.1',
+    version='0.0.3',
     author='Tristan King',
     author_email='tristan.king@gmail.com',
     packages=['bbtornado'],
