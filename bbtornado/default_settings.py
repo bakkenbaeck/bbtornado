dd484525d61bebb7586e8a1194d2a6f54eec9899
Tristan King <tristan.king@gmail.com>
2014-03-31 11:39:21 +0200

    changed config settings

--- bbtornado/default_settings.py
+++ bbtornado/default_settings.py
@@ -4,7 +4,11 @@ import json
 SQLALCHEMY_DATABASE_URI = 'sqlite:///%s'%join(dirname(abspath(__file__)), pardir, 'database.db')
 SECRET_KEY = 'something not very random'
 DEBUG = True
-try:
+if exists('../config.json'):
+    _config = json.load(open('../config.json', 'r'))
+    BASE_URL = _config['baseUrl'] if _config.has_key('baseUrl') else ""
+    DEBUG = _config['debug'] if _config.has_key('baseUrl') else True
+elif exists('../baseURL.json'): # depreciated!
     BASE_URL = json.load(open('../baseURL.json', 'r'))
-except IOError:
+else:
     BASE_URL = ""
