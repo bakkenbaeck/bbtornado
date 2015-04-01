d2205100a9268f97685356b0425d1a714394d164
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2015-04-01 14:07:24 +0200

    added today method

--- bbtornado/utils.py
+++ bbtornado/utils.py
@@ -4,3 +4,9 @@ import dateutil.tz
 def now():
     """ A datetime of now with timezone """
     return datetime.now(dateutil.tz.tzutc())
+
+def today():
+    """ A datetime of midnight today"""
+
+    now = datetime.now(dateutil.tz.tzutc())
+    return datetime(now.year, now.month, now.day, tzinfo=now.tzinfo)
