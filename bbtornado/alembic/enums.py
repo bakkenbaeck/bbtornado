ebd44bb91353caaf65ec1caa32bbcca20e6f8e5b
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2016-09-01 10:55:10 +0200

    tweaks to enum support functions

--- bbtornado/alembic/enums.py
+++ bbtornado/alembic/enums.py
@@ -7,11 +7,29 @@ migration scripts.
 
 This assumes a postgresql server
 
+Both creation/adding options will gracefully handle enum/options
+already existing.
+
+Downgrading is not supported.
+
+This requires that you set `transaction_per_migration=True` in the config clause for alembic:
+
+i.e. in the env.py file:
+
+```
+    context.configure(
+        transaction_per_migration=True,
+        connection=connection,
+        target_metadata=target_metadata
+    )
+```
+
+
 """
 
 log = logging.getLogger(__file__)
 
-def create(op, name, values):
+def create(op, name, *values):
 
     enum = ENUM(*values, name=name, create_type=False)
     enum.create(op.get_bind(), checkfirst=True)
@@ -19,7 +37,7 @@ def create(op, name, values):
     return enum
 
 
-def add_option(op, name, option):
+def add_options(op, name, *options):
 
     log.warn("This will roll back any upgrades done before this!")
 
@@ -28,7 +46,8 @@ def add_option(op, name, option):
         connection = op.get_bind()
         connection.execution_options(isolation_level='AUTOCOMMIT')
 
-    op.execute("ALTER TYPE %s ADD VALUE '%s'"%(name, option))
+    for option in options:
+        op.execute("ALTER TYPE %s ADD VALUE IF NOT EXISTS '%s'"%(name, option))
 
     if connection is not None:
         connection.execution_options(isolation_level='READ_COMMITTED')
@@ -37,3 +56,6 @@ def add_option(op, name, option):
 def remove_option(op, name, option):
 
     raise Exception("lol - good luck!")
+
+def existing(name):
+    return ENUM(name=name, create_type=False)
