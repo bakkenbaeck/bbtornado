4ca06762328860980e7fb96694f0627c8fc94501
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2016-10-04 14:46:34 +0200

    don't depend on a particular version of tornado
    run with scissors instead!

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     url='http://github.com/bakkenbaeck/bbtornado',
     description='Basic app setup using tornado.',
     long_description=open('README.md').read(),
-    install_requires = [ 'tornado==4.2.1','futures','sqlalchemy' ]
+    install_requires = [ 'tornado','futures','sqlalchemy' ]
 )
