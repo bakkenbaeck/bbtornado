734948c7031f15e3ecac2576b21722f5227ac09f
Christoph Reimann <christoph@bakkenbaeck.no>
2017-05-30 11:38:32 +0200

    Make jsonschema optional dependency

--- setup.py
+++ setup.py
@@ -9,5 +9,8 @@ setup(
     url='http://github.com/bakkenbaeck/bbtornado',
     description='Basic app setup using tornado.',
     long_description=open('README.md').read(),
-    install_requires=['tornado', 'futures', 'sqlalchemy', 'jsonschema']
+    install_requires=['tornado', 'futures', 'sqlalchemy'],
+    extras_require={
+        'jsonschema': ['jsonschema']
+    }
 )
