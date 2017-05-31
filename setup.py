e68f6f83dcc29a01fbbf20b9dcdd1973af9192ae
Anders <anders@bakkenbaeck.no>
2017-05-31 15:18:16 +0200

    Added `six` as dependency.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='http://github.com/bakkenbaeck/bbtornado',
     description='Basic app setup using tornado.',
     long_description=open('README.md').read(),
-    install_requires=['tornado', 'futures', 'sqlalchemy'],
+    install_requires=['tornado', 'futures', 'sqlalchemy', 'six'],
     extras_require={
         'jsonschema': ['jsonschema']
     }
