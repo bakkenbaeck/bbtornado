e00a932fd2d18746716e125a1307462939e3c804
Marius Feldmann <feldmann.marius@gmail.com>
2017-07-27 15:24:03 +0200

    Added function to count the results of a sqlalchemy query

--- bbtornado/utils.py
+++ bbtornado/utils.py
@@ -1,12 +1,19 @@
 from datetime import datetime
 import dateutil.tz
+from sqlalchemy import func
 
 def now():
-    """ A datetime of now with timezone """
+    """A datetime of now with timezone"""
     return datetime.now(dateutil.tz.tzutc())
 
 def today():
-    """ A datetime of midnight today"""
-
+    """A datetime of midnight today"""
     now = datetime.now(dateutil.tz.tzutc())
     return datetime(now.year, now.month, now.day, tzinfo=now.tzinfo)
+
+def count_results(q):
+    """Returns the count for the supplied sqlalchemy query"""
+    # https://gist.github.com/hest/8798884
+    count_q = q.statement.with_only_columns([func.count()]).order_by(None)
+    count = q.session.execute(count_q).scalar()
+    return count
