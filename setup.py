f8558f49133ec87ef34bb257ba46405b22cd67fb
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2017-10-20 10:43:21 +0200

    read version number from __init__
    increase to 0.0.5

--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
 from setuptools import setup
 
+import re
+import io
+import os.path
+
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name='BBTornado',
-    version='0.0.4',
+    version=find_version('bbtornado', '__init__.py'),
     author='Tristan King',
     author_email='tristan.king@gmail.com',
     packages=['bbtornado', 'bbtornado.alembic'],
