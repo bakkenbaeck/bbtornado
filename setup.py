62c42223eaf02c51a393cfdc71836a0897176377
Philipp Gross <philipp@bakkenbaeck.no>
2017-10-23 13:40:49 +0200

    Added missing PyYAML dependency

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     url='http://github.com/bakkenbaeck/bbtornado',
     description='Basic app setup using tornado.',
     long_description=open('README.md').read(),
-    install_requires=['tornado', 'futures', 'sqlalchemy', 'six', 'python-dateutil'],
+    install_requires=['tornado', 'futures', 'sqlalchemy', 'six', 'python-dateutil', 'PyYAML'],
     extras_require={
         'jsonschema': ['jsonschema']
     }
